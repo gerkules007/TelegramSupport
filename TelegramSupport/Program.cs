540f85d3f9061d4666a59945a9652a0af746c7d6
OvcS <gerkules007@yahoo.com>
2022-05-08 21:35:39 +0300

    Class TelegramNetwork, GetRequest()

--- TelegramSupport/Program.cs
+++ TelegramSupport/Program.cs
@@ -1,9 +1,24 @@
-﻿class Programm
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+
+namespace TelegramSupport
 {
-    static void Main(string[] args)
+    class Programm
     {
-        Console.WriteLine("Initial commit");
+        static void Main(string[] args)
+        {
+            string path = $"C:\\Users\\ovcse\\Desktop\\РАЗРАБОТЧИК\\ФАКУЛЬТАТИВЫ\\С# Telegram\\TelegramSupport\\TelegramSupport\\token.txt";
+            string TOKEN = File.ReadAllText(path);
+
+            TelegramNetwork tn = new(TOKEN);
+
+            tn.GetRequest();
+            Console.WriteLine(tn.TextAnswer);
+        }
     }
 }
+
 
 
